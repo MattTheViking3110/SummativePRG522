0f45efe2fc54c60e2edec6753fb978db6644228d
MattTheViking3110 <88708780+MattTheViking3110@users.noreply.github.com>
2021-09-15 14:03:10 +0200

    Add project files.

---
+++ SummativePRG522/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SummativePRG522.MvcApplication" Language="C#" %>
---
+++ SummativePRG522/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SummativePRG522.Startup))]
+namespace SummativePRG522
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
